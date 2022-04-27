af44ec727dd1060fb12afccd4e8341b05617caf7
Lucas Huertas <lucashuertas1993@gmail.com>
2022-04-27 00:22:42 -0300

    Progreso

--- Guia4-Lab3/src/Models/VideoClub.java
+++ Guia4-Lab3/src/Models/VideoClub.java
@@ -1,5 +1,6 @@
 package Models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class VideoClub {
@@ -9,4 +10,53 @@ public class VideoClub {
     private List<Alquiler> listaAlquileres;
 
     public VideoClub(){}
+
+    public VideoClub(List<Pelicula> peliculas, List<Cliente> clientes){
+        this.listaPeliculas = peliculas;
+        this.listaClientes = clientes;
+        this.listaAlquileres = new ArrayList<>();
+    }
+
+    public List<Pelicula> getListaPeliculas() {
+        return listaPeliculas;
+    }
+    public void setListaPeliculas(List<Pelicula> listaPeliculas) {
+        this.listaPeliculas = listaPeliculas;
+    }
+    public List<Cliente> getListaClientes() {
+        return listaClientes;
+    }
+    public void setListaClientes(List<Cliente> listaClientes) {
+        this.listaClientes = listaClientes;
+    }
+    public List<Alquiler> getListaAlquileres() {
+        return listaAlquileres;
+    }
+    public void setListaAlquileres(List<Alquiler> listaAlquileres) {
+        this.listaAlquileres = listaAlquileres;
+    }
+
+    public boolean existePelicula(Pelicula pelicula){
+        boolean encontrada = false;
+        if (listaPeliculas != null) {
+            for (Pelicula peli : listaPeliculas) {
+                if (peli.getTitulo() == pelicula.getTitulo())
+                    encontrada = true;
+            }
+        }
+        return encontrada;
+    }
+
+    public boolean tieneStock (String titulo){
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "VideoClub{" +
+                "listaPeliculas=" + listaPeliculas +
+                ", listaClientes=" + listaClientes +
+                ", listaAlquileres=" + listaAlquileres +
+                '}';
+    }
 }
